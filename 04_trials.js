65350eb29f1c3fdb3b0c6cef84238f9deba89592
krandiash <kgoel93@gmail.com>
2020-12-19 15:39:39 -0800

    dummy data

--- 04_trials.js
+++ 04_trials.js
@@ -19,3 +19,32 @@ const trial_info = {
         }
     ]
 };
+
+
+const example_ned_info = {
+    forced_choice: [
+        {
+            question: "What's the right entity?",
+            sentence: "David and Victoria couldn't wait to be on QI.",
+            spans: [[0, 4], [10, 17], [43, 44]],
+            annotations: ['PERSON', 'PERSON', 'TV SHOW'],
+            candidates: [
+                [
+                    ['Q123', 'David Beckham', 'DB is'],
+                    ['Q234', 'David Mitchell', 'DM is'],
+                    ['Q345', 'David Wallace', 'DW is']
+                ],
+                [
+                    ['Q987', 'Victoria Beckham', 'VB is'],
+                    ['Q876', 'Victoria Coren Mitchell', 'VCM is'],
+                    ['Q765', 'Victoria Cross', 'VC is'],
+                    ['Q654', 'Queen Victoria', 'QV is'],
+                ],
+                [
+                    ['Q1010', 'Quite Interesting', 'QI is'],
+                    ['Q3030', 'Qi', 'Qi is']
+                ],
+            ],
+        }
+    ]
+};
