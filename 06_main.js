13a338bd1ace4a258f061c13e4a09d48da93069d
lorr1 <57237365+lorr1@users.noreply.github.com>
2020-12-22 13:42:18 -0700

    SEcond

--- 06_main.js
+++ 06_main.js
@@ -16,7 +16,7 @@ $("document").ready(function() {
         // You have to specify all views you want to use in this experiment and the order of them
         views_seq: [
             // intro,
-            // instructions,
+            instructions,
             entity_choice,
             entity_choice_gld,
             // post_test,
