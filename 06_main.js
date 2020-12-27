932e8adab99f602b327b3130920423c90c48184f
lorr1 <57237365+lorr1@users.noreply.github.com>
2020-12-27 13:30:33 -0700

    Update

--- 06_main.js
+++ 06_main.js
@@ -29,7 +29,7 @@ $("document").ready(function() {
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "MTurkSandbox",
+            deployMethod: "MTurk",
             contact_email: "lorr1@cs.stanford.edu",
             // prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
